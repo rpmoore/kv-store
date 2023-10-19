c2c6ad996f6f45368fc4104d85f3535c6a1d30fe
Ryan Moore <ryan.moore@greyrocksoft.com>
2023-10-19 14:07:54 -0600

    put a rest interface in front of the grpc calls

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,18 +1,3 @@
 pub mod storage {
     tonic::include_proto!("storage"); // The string specified here must match the proto package name
 }
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
--- kvstore/src/main.rs
+++ kvstore/src/main.rs
@@ -1,19 +1,96 @@
 use common::storage::{storage_client::StorageClient, PutRequest};
 use tonic;
 use actix_web;
+use actix_web::{Responder, web, HttpServer, App, put, HttpResponse, body::BoxBody, HttpRequest, http::header::ContentType, error};
+use actix_web::http::StatusCode;
+use serde::{Deserialize, Serialize};
+use derive_more::{Display, Error};
 
 #[actix_web::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = StorageClient::connect("http://[::1]:50051").await?;
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(put))
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
+}
+
+#[derive(Deserialize)]
+struct PutValue {
+    value: String,
+    crc: Option<u32>,
+}
+
+#[derive(Serialize)]
+struct PutResp {
+    version: u32,
+    crc: u32,
+    creation_time: String,
+}
+
+impl Responder for PutResp {
+    type Body = BoxBody;
+
+    fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
+        let body = serde_json::to_string(&self).unwrap();
+
+        // Create response and set content type
+        HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .body(body)
+    }
+}
+
+#[derive(Error, Display, Debug)]
+enum KVErrors {
+    #[display(fmt="downstream service unavailable")]
+    ServiceUnavailable,
+
+    #[display(fmt="internal server error")]
+    InternalServerError
+}
+
+impl error::ResponseError for KVErrors {
+    fn status_code(&self) -> StatusCode {
+        match *self {
+            KVErrors::InternalServerError => StatusCode::INTERNAL_SERVER_ERROR,
+            KVErrors::ServiceUnavailable => StatusCode::SERVICE_UNAVAILABLE,
+        }
+    }
+
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code())
+            .insert_header(ContentType::plaintext())
+            .body(self.to_string())
+    }
+}
+
+#[put("/key/{id}")]
+async fn put(path: web::Path<String>, data: web::Json<PutValue>) -> Result<impl Responder, KVErrors> {
+    let id = path.into_inner();
+
+    let result = StorageClient::connect("http://[::1]:50051").await;
+
+    let mut client = match result {
+        Ok(client) => client,
+        Err(_) => return Err(KVErrors::ServiceUnavailable),
+    };
+
+    let value = data.into_inner();
 
     let request = tonic::Request::new(PutRequest {
-        key: "key".as_bytes().to_vec(),
-        value: "value".as_bytes().to_vec(),
-        crc: Some(12)
+        key: id.into_bytes(),
+        value: value.value.into_bytes(),
+        crc: value.crc,
     });
 
-    let response = client.put(request).await?;
-    println!("RESPONSE={:?}", response);
+    let put_response = match client.put(request).await {
+        Ok(response) => response.into_inner(),
+        Err(_) => return Err(KVErrors::InternalServerError)
+    };
 
-    Ok(())
+    Ok(PutResp{
+        version: put_response.version,
+        crc: put_response.crc,
+        creation_time: put_response.creation_time.map_or(String::from(""), |timestamp| timestamp.to_string())
+    })
 }
