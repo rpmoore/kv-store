a590d7462c15ced78ebfdc3ad8c0f6dcbdc5eb09
Ryan Moore <ryan.moore@greyrocksoft.com>
2026-05-04 23:08:07 -0600

    updating some dependencies and changing how prost is used since they changed a few things with one of the updates

--- common/build.rs
+++ common/build.rs
@@ -1,8 +1,8 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .build_server(true)
         .build_client(true)
         .protoc_arg("--experimental_allow_proto3_optional")
-        .compile(&["proto/storage.proto", "proto/admin.proto"], &["proto"])?;
+        .compile_protos(&["proto/storage.proto", "proto/admin.proto"], &["proto"])?;
     Ok(())
 }
